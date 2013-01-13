1fd7e43286fbf89d5a9ab4164785ef9d6d62ce71
jgoode21 <jimmiegoode@gmail.com>
2013-01-13 17:14:45 -0500

    Fixed MPI groupings

--- src/Backtest.cpp
+++ src/Backtest.cpp
@@ -14,12 +14,19 @@ void checkErr(int i) {
   }
 }
 
+void printRange(int r[][3], string name) {
+  int i = 0;
+  cout << name << " = [" << r[i][0] << ", " << r[i][1] << ", " << r[i][2] << "]" << endl;
+}
+
 //-------------------------------------------------------------------------------
 
-int main(int argc, char **argv) {
+int main4(int argc, char **argv) {
 
-  int rank[2], size[2], namelen, xranks[] = { 0 };
-  int iranks[] = { 1 };
+  int rank[2], size[2], namelen;
+  int xranks[] = { 0 };
+  int iranks[] = { 6 };
+  int rranks[][3] = {6,6,1};
 
   char processor_name[MPI_MAX_PROCESSOR_NAME];
 
@@ -37,16 +44,19 @@ int main(int argc, char **argv) {
   MPI_Comm_group(MPI_COMM_WORLD, &group_world);
 
   //MPI_Group_excl(group_world, 1, xranks, &group_garch);
-  MPI_Group_incl(group_world, 1, iranks, &group_garch);
+  //MPI_Group_incl(group_world, 1, iranks, &group_garch);
+  MPI_Group_range_incl(group_world, 1, rranks, &group_garch);
 
   MPI_Comm_create(MPI_COMM_WORLD, group_garch, &comm_garch);
 
-  printf("Hello world! I’m rank %d of %d on %s\n", rank[0], size[0], processor_name);
+  printf("Hello world! I'm rank %d of %d on %s\n", rank[0], size[0], processor_name);
 
-  if (rank[0]) {
-	int rank1;
-	MPI_Comm_rank(comm_garch, &rank1);
-	cout << "rank1 = " << rank1 << endl;
+  // If this rank is in the subgroup, get it's subgroup rank.
+  if (rank[0] == 6) {
+	MPI_Comm_rank(comm_garch, &rank[1]);
+	MPI_Comm_size(comm_garch, &size[1]);
+	cout << "rank[0] = " << rank[0] << ", rank[1] = " << rank[1] << endl;
+	cout << "size[0] = " << size[0] << ", size[1] = " << size[1] << endl;
   }
 
  if (comm_garch != MPI_COMM_NULL)
@@ -63,72 +73,67 @@ int main(int argc, char **argv) {
 
 //-------------------------------------------------------------------------------
 
-int main3(int argc, char **argv) {
+int main(int argc, char **argv) {
 
   // rank of this process
-  int rank[2], ie;
+  int rank, ie;
 
   // sizes of each group
   int size_world, size_garch, size_mat;
 
   // set group sizes <TODO>
-  size_garch = 1;
-  size_mat = 1;
+  //size_garch = 6;
+  //size_mat = 1;
   
   MPI_Group group_world, group_garch, group_mat;
   MPI_Comm comm_garch, comm_mat;
 
   MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank[0]);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size_world);
 
+  size_garch = size_world - 1;
+  size_mat = 1;
+  
   // check that group sizes sum to total world size
   assert(size_world == (size_garch + size_mat));
 
   // Set the inclusion rank arrays of form {minRank, maxRank, stride}.
   // The ordering is global = {garch, mat}.
   int range_garch[][3] = {0, size_garch-1, 1};
-  ///int range_mat[][3] = {size_garch, size_garch+size_mat-1, 1};
+  int range_mat[][3] = {size_garch, size_garch+size_mat-1, 1};
+
+  if (rank == 0) {
+	printRange(range_garch, "range_garch");
+	printRange(range_mat, "range_mat");
+  }
 
   // set all groups
   MPI_Comm_group(MPI_COMM_WORLD, &group_world);
-
-  if (range_garch[0][0] == range_garch[0][1]) {
-	cout << "One processor for garch\n";
-	
-	int rnk[] = {1};
-	//MPI_Group_incl(group_world, 2, rnk, &group_garch);
-	MPI_Group_excl(group_world, 1, rnk, &group_garch);
-	
-  } else {
-
-	//ie = MPI_Group_range_incl(group_world, 1, range_garch, &group_garch);
-	assert(0 && "---> FAIL");
-  }
-  
-  //// ie = MPI_Group_range_incl(group_world, 1, range_mat, &group_mat);
+  MPI_Group_range_incl(group_world, 1, range_garch, &group_garch);
+  MPI_Group_range_incl(group_world, 1, range_mat, &group_mat);
   
   // set communicators for each non-world group
-  ie = MPI_Comm_create(MPI_COMM_WORLD, group_garch, &comm_garch);
-  //// MPI_Comm_create(MPI_COMM_WORLD, group_mat, &comm_mat);
+  MPI_Comm_create(MPI_COMM_WORLD, group_garch, &comm_garch);
+  MPI_Comm_create(MPI_COMM_WORLD, group_mat, &comm_mat);
 
-  // <TODO>
-  // - Load HDF files into DistMat
-  // - Start backtest
-
-  if (rank[0]) {
-	int rank1, rank2;
-	//MPI_Comm_rank(comm_garch, &rank1);
-	//MPI_Comm_rank(comm_garch, &rank2);
-	cout << "rank1 = " << rank1 << ", rank2 = " << rank2 << endl;
+  int grank, gsize;
+  if (rank < size_garch) {
+	MPI_Comm_rank(comm_garch, &grank);
+	MPI_Comm_size(comm_garch, &gsize);
+	cout << "GARCH Member: ranks = [" << rank << ", " << grank << "], groupSize = " << gsize << endl;
+  } else if (rank >= size_garch) {
+	MPI_Comm_rank(comm_mat, &grank);
+	MPI_Comm_size(comm_mat, &gsize);
+	cout << "MAT Member: ranks = [" << rank << ", " << grank << "], groupSize = " << gsize << endl;
   }
   
   // free all communicators
   if (comm_garch != MPI_COMM_NULL) MPI_Comm_free(&comm_garch);
-  //// if (comm_mat != MPI_COMM_NULL) MPI_Comm_free(&comm_mat);
+  if (comm_mat != MPI_COMM_NULL) MPI_Comm_free(&comm_mat);
 
   // free all groups
-  //// MPI_Group_free(&group_mat);
+  MPI_Group_free(&group_mat);
   MPI_Group_free(&group_garch);
   MPI_Group_free(&group_world);
 
