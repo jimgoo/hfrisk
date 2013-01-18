bfb31b50785ceff6d36f1cf24a90c5540672d300
jgoode21 <jimmiegoode@gmail.com>
2013-01-18 13:37:38 -0500

    Mac updates

--- src/run_mac.sh
+++ src/run_mac.sh
@@ -6,12 +6,12 @@ CG_EXP="${CG_DATA}/exports"
 mpirun -np 2 ../bin/HFRisk \
 	-x OMP_NUM_THREADS \
 	-df "${CG_DATA}/csi_20030101_20120801_v3" \
-	-rf "${CG_EXP}/ST_5109" \
+	-rf "${CG_EXP}/AS_5109" \
 	-mc -1 \
 	-nSims 10000 \
 	-v 1 \
 	-margOnly 0 \
-	-innovType 2 \
+	-innovType 1 \
 	-depStruct 1 \
 	-doCheckEigs 0 \
 	-beginDate 20080102 \
