e6a13c49f456ca02992759f4fec67ae2b57d9045
Jimmie Goode <jimmie21@dontpanic.(none)>
2013-01-25 18:44:12 -0500

    vogon mods

--- src/batch_chol.sh
+++ src/batch_chol.sh
@@ -1,7 +1,7 @@
 #PBS -N Cholesky
 #PBS -l nodes=1:towel:ppn=12
-#PBS -o pbs_output.out
-#PBS -e pbs_error.out
+#PBS -o /nfs/user01/jimmie21/pbs/pbs_output.out
+#PBS -e /nfs/user01/jimmie21/pbs/pbs_error.out
 ### send me email when job begins
 #PBS -m b
 ### send me email when job end
@@ -9,15 +9,33 @@
 ### send me email when job aborts (with an error)
 #PBS -m a
 
-D=100000
-NP=12
+D=5000
 
 cd /nfs/user01/jimmie21/github/hfrisk/src
- 
+
 LIBDIR=/nfs/user01/jimmie21/github/hfrisk/lib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${LIBDIR}/gsl-1.15/install/gsl/lib:${LIBDIR}/nlopt-2.3/install/lib:${LIBDIR}/armadillo-3.6.1/install/usr/lib64:${LIBDIR}/fftw-3.3.2/installdir/lib:${LIBDIR}/hdf5-1.8.10/install/lib
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${LIBDIR}/gsl-1.15/install/gsl/lib:${LIBDIR}/nlopt-2.3/install/lib:${LIBDIR}/armadillo-3.6.1/install/usr/lib64:${LIBDIR}/fftw-3.3.2/installdir/lib:${LIBDIR}/hdf5-1.8.10/install/lib
 
-OUTFILE=/nfs/user01/jimmie21/github/hfrisk/src/runTimes/chol_d=${D}_np=${NP}.txt
+# # NP=2
+# # OUTFILE=/nfs/user01/jimmie21/github/hfrisk/src/runTimes/chol_d=${D}_np=${NP}.txt
+# # mpirun -np ${NP} ../bin/Cholesky -n ${D} > ${OUTFILE}
 
+# # NP=4
+# # OUTFILE=/nfs/user01/jimmie21/github/hfrisk/src/runTimes/chol_d=${D}_np=${NP}.txt
+# # mpirun -np ${NP} ../bin/Cholesky -n ${D} > ${OUTFILE}
+
+# # NP=6
+# # OUTFILE=/nfs/user01/jimmie21/github/hfrisk/src/runTimes/chol_d=${D}_np=${NP}.txt
+# # mpirun -np ${NP} ../bin/Cholesky -n ${D} > ${OUTFILE}
+
+# # NP=8
+# # OUTFILE=/nfs/user01/jimmie21/github/hfrisk/src/runTimes/chol_d=${D}_np=${NP}.txt
+# # mpirun -np ${NP} ../bin/Cholesky -n ${D} > ${OUTFILE}
+
+# # NP=10
+# # OUTFILE=/nfs/user01/jimmie21/github/hfrisk/src/runTimes/chol_d=${D}_np=${NP}.txt
+# # mpirun -np ${NP} ../bin/Cholesky -n ${D} > ${OUTFILE}
+
+NP=2
+OUTFILE=/nfs/user01/jimmie21/github/hfrisk/src/runTimes/chol.txt
 mpirun -np ${NP} ../bin/Cholesky -n ${D} > ${OUTFILE}
-
