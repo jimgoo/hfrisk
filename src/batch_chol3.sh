a3877435e633777f8b0794d708e3f4c512416843
Jimmie Goode <jimmie21@dontpanic.(none)>
2013-01-30 16:35:51 -0500

    vogon changes

--- src/batch_chol3.sh
+++ src/batch_chol3.sh
@@ -1,5 +1,5 @@
 #PBS -N batch_chol3
-#PBS -l nodes=4:towel:ppn=12
+#PBS -l nodes=8:towel:ppn=12
 #PBS -o /dev/null
 #PBS -e /dev/null
 
@@ -11,9 +11,27 @@ LIBDIR=/nfs/user01/jimmie21/github/hfrisk/lib
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${LIBDIR}/gsl-1.15/install/gsl/lib:${LIBDIR}/nlopt-2.3/install/lib:${LIBDIR}/armadillo-3.6.1/install/usr/lib64:${LIBDIR}/fftw-3.3.2/installdir/lib:${LIBDIR}/hdf5-1.8.10/install/lib
 
-OUTDIR=/nfs/user01/jimmie21/github/hfrisk/src/runTimes2
+OUTDIR=/nfs/user01/jimmie21/github/hfrisk/src/runTimes3
+
+mkdir ${OUTDIR}
 
 NP=48
+OUTFILE="${OUTDIR}/chol_d=${D}_np=${NP}.txt"
+mpirun -np ${NP} ../bin/Cholesky -n ${D} > ${OUTFILE}
+
+NP=60
+OUTFILE="${OUTDIR}/chol_d=${D}_np=${NP}.txt"
+mpirun -np ${NP} ../bin/Cholesky -n ${D} > ${OUTFILE}
+
+NP=72
+OUTFILE="${OUTDIR}/chol_d=${D}_np=${NP}.txt"
+mpirun -np ${NP} ../bin/Cholesky -n ${D} > ${OUTFILE}
+
+NP=84
+OUTFILE="${OUTDIR}/chol_d=${D}_np=${NP}.txt"
+mpirun -np ${NP} ../bin/Cholesky -n ${D} > ${OUTFILE}
+
+NP=96
 OUTFILE="${OUTDIR}/chol_d=${D}_np=${NP}.txt"
 mpirun -np ${NP} ../bin/Cholesky -n ${D} > ${OUTFILE}
 
